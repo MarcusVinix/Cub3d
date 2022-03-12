34bff846e570c0e7a0a983bce983b0c44a5c6635
MarcusVinix <vi.ni159marcus@gmail.com>
2022-03-12 17:57:53 +0100

    arrumando segfault ao clicar no x da janela e colocando action para pegar apenas as pastas necessariass

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: mavinici <mavinici@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 01:21:23 by mavinici          #+#    #+#             */
-/*   Updated: 2022/03/12 17:43:29 by mavinici         ###   ########.fr       */
+/*   Updated: 2022/03/12 17:55:22 by mavinici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	start_game(t_cub *cub)
 	cub->s_mlx.win = mlx_new_window(cub->s_mlx.mlx, WIDTH, HEIGHT, "cub3D");
 	build_main_img(cub);
 	setup(cub);
-	mlx_hook(cub->s_mlx.win, 17, 0, close_win, (void *)&cub);
+	mlx_hook(cub->s_mlx.win, 17, 0, close_win, (void *)cub);
 	mlx_hook(cub->s_mlx.win, 2, 1L << 0, action, (void *)cub);
 	mlx_loop_hook(cub->s_mlx.mlx, action_loop, (void *)cub);
 	mlx_loop(cub->s_mlx.mlx);
